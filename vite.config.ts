2cc6ae7c88c0a71da2a26927db2ab873f60efdbc
AK <i.zhenlong@icloud.com>
2025-09-06 18:48:59 +0900

    deploy: rebuild with correct Vite base /enchat/

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// GitHub Pages (プロジェクトサイト) 用に必須: /<repo名>/
-export default defineConfig({
-  plugins: [react()],
-  base: '/enchat/',
-})
